43d7ad2227a6cb52d6161ca8a936f68d645da132
Ashish Varghese M <ashgeorge009@gmail.com>
2020-08-14 20:14:29 +0530

    sticky basic

--- tools.js
+++ tools.js
@@ -4,8 +4,14 @@ let undo = document.querySelector("#undo");
 let redo = document.querySelector("#redo");
 let pencilOptions = document.querySelector("#pencil-options");
 let eraserOptions = document.querySelector("#eraser-options");
+let sliders = document.querySelectorAll("input[type='range']");
+let sticky = document.querySelector("#sticky");
 // ctx.lineWidth=10;
 let activeTool = "pencil";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.miterLimit = 1;
+
 pencil.addEventListener("click", function () {
     if (activeTool == "pencil") {
         //  pencil options show
@@ -32,6 +38,11 @@ undo.addEventListener("click", function () {
 redo.addEventListener("click", function () {
     redoMaker();
 })
+
+sticky.addEventListener("click", function () {
+    createSticky();
+})
+
 document.addEventListener("keydown", function (e) {
     var evtobj = e;
     if (evtobj.keyCode == 90 && evtobj.ctrlKey)
@@ -46,3 +57,10 @@ document.addEventListener("keydown", function (e) {
 function handleColor(color) {
     ctx.strokeStyle = color;
 }
+
+sliders.forEach(function (slider) {
+    slider.addEventListener("change", function () {
+        let value = slider.value;
+        ctx.lineWidth = value;
+    })
+})
